d88f6f63c416174ea5cb373444955ae5ed601406
jitendraT <mail.jitendra05@gmail.com>
2025-04-06 01:11:07 +0530

    Added log

--- src/main/java/com/batch/controller/TriggerJobController.java
+++ src/main/java/com/batch/controller/TriggerJobController.java
@@ -1,17 +1,19 @@
 package com.batch.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TriggerJobController {
 
+    private static final Log log = LogFactory.getLog(TriggerJobController.class);
     @Autowired
     JobLauncher jobLauncher;
 
@@ -19,6 +21,7 @@ public class TriggerJobController {
     Job processJob;
     @GetMapping("/v1/invoke/job")
     public String handle() throws Exception {
+        log.info("Received request to start batch job");
         JobParameters jobParameters = new JobParametersBuilder()
                 .addLong("time", System.currentTimeMillis())
                 .toJobParameters();
